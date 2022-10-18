260800e40fdddb4a7a214a1ada3e24ad04265201
Swank <willmadedesign@gmail.com>
2022-10-18 16:57:13 -0700

    Set up js DOM functions

---
+++ main.js
@@ -0,0 +1,20 @@
+const addBook = document.querySelector('.add-book');
+const grid = document.querySelector('.books-grid');
+const form = document.querySelector('.add-book-form');
+const modal = document.querySelector('.modal');
+let myLibrary = [];
+
+
+
+//psuedo code 
+
+//ACCESSING THE MODAL
+//when the add book button is pressed
+//make the modal with the form appear on the document
+
+
+
+//CREATING THE CARD
+//when the add new book button is pressed
+//verify all fields are filled 
+//then add the new card with all the information to the book grid

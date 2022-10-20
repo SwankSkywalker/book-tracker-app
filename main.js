785499bce250e39bd7a61c81c791b28519399aa8
Swank <willmadedesign@gmail.com>
2022-10-19 17:50:04 -0700

    Refactor script

--- main.js
+++ main.js
@@ -1,26 +1,41 @@
+//Construct Classes
 
+class Book {
+    constructor(
+        title = 'Unknown',
+        author = 'Unknown',
+        pages = 0,
+        isRead = false
+    ) {
+        this.title = title
+        this.author = author
+        this.pages = pages
+        this.isRead = isRead
+    }
+}
 
+class Shelf {
+    constructor() {
+        this.books = []
+    }
 
-//psuedo code 
+    addBook(newBook) {
+        if (!this.isInLibrary(newBook)) {
+            this.books.push(newBook)
+        }
+    }
 
-function library() {
-    let modal = document.querySelector('.modal');
-    let newBook = document.querySelector('.add-book');
-    
-    //ACCESSING THE MODAL
-     newBook.addEventListener('click', function() {
-         modal.classList.add('show');
-     });
-     
-     //CREATING THE CARD
-     
- }
- 
- library();
+    removeBook(title) {
+        this.books = this.books.filter((book) => book.title !== title)
+    }
 
+    getBook(title) {
+        return this.books.find((book) => book.title === title)
+    }
 
-//when the add new book button is pressed
-//verify all fields are filled 
-//then add the new card with all the information to the book grid
+    isInLibrary(newBook) {
+        return this.books.some((book) => book.title === newBook.title)
+    }
+}
 
-
+const shelf = new Shelf()

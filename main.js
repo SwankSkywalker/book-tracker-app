26b03cbccd95c4ef9f2c0f06e1ceb750bf76374d
Swank <willmadedesign@gmail.com>
2022-10-19 18:44:18 -0700

    Open/close Modal

--- main.js
+++ main.js
@@ -39,3 +39,27 @@ class Shelf {
 }
 
 const shelf = new Shelf()
+
+const loginBtn = document.getElementById('logInBtn');
+const addBookBtn = document.getElementById('newBook');
+const bookGrid = document.querySelector('.books-grid');
+const bookModal = document.getElementById('book-modal');
+const bookForm = document.getElementById('addBookForm');
+const overlay = document.getElementById('overlay');
+
+
+//Making Book modal show
+
+const openNewBookModal = () => {
+    bookForm.reset()
+    bookModal.classList.add('show'); 
+    overlay.classList.add('active');
+}
+
+const closeNewBookModal = () => {
+    bookModal.classList.remove('show');
+    overlay.classList.remove('active');
+}
+
+addBookBtn.addEventListener('click', openNewBookModal);
+overlay.addEventListener('click', closeNewBookModal);
